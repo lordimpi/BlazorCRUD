fc3bed3b8258f25486e7512372bcda29e3c2d077
Santiago Acuña <snt-26@hotmail.com>
2020-09-16 14:04:35 -0500

    Add FilmRepository class and dbConnection

---
+++ BlazorCRUD.Data.Dapper/Repositories/FilmRepository.cs
@@ -0,0 +1,49 @@
+﻿using BlazorCRUD.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorCRUD.Data.Dapper.Repositories
+{
+    public class FilmRepository : IFilmRepository
+    {
+        private string ConnectionString;
+
+        public FilmRepository(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        protected SqlConnection DbConnection()
+        {
+            return new SqlConnection(ConnectionString);
+        }
+
+        public Task<bool> DeleteFilm()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Film>> GetAllFilms()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Film> GetFilmDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> InsertFilm()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateFilm()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BlazorCRUD.Data.Dapper/Repositories/IFilmRepository.cs
@@ -0,0 +1,15 @@
+﻿using BlazorCRUD.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorCRUD.Data.Dapper.Repositories
+{
+    public interface IFilmRepository
+    {
+        Task<IEnumerable<Film>> GetAllFilms();
+        Task<Film> GetFilmDetails();
+        Task<bool> InsertFilm();
+        Task<bool> UpdateFilm();
+        Task<bool> DeleteFilm();
+    }
+}
